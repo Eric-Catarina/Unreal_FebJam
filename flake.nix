642e9ccdfc3530fc70a9d53cdbb42e2b98d30a95
niscolas <niscolas@proton.me>
2024-09-27 19:33:59 -0300

    misc: updates flake setup

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
     mach-nix.url = "mach-nix/3.5.0";
   };
 
@@ -18,19 +19,15 @@
     systems,
     ...
   } @ inputs: let
-    l = inputs.nixpkgs.lib // builtins;
-    supportedSystems = ["x86_64-linux" "aarch64-darwin"];
-    forEachSystem = f:
-      l.genAttrs supportedSystems
-      (system: f system (import inputs.nixpkgs {inherit system;}));
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
-    packages = forEachSystem (system: pkgs: {
+    packages = forEachSystem (system: {
       devenv-up = self.devShells.${system}.default.config.procfileScript;
     });
 
     devShells =
       forEachSystem
-      (system: pkgs: let
+      (system: let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
@@ -42,83 +39,26 @@
           '';
         };
 
-        devenvCustomDirPath = "./devenv-custom";
-        projectName = "MonoGrinding";
         pwd = builtins.getEnv "PWD";
-        ueBatchFilesDirPath = "${ueRootDirPath}/Engine/Build/BatchFiles/Linux";
-        ueCentosBinariesDirPath = "${ueRootDirPath}/Engine/Extras/ThirdPartyNotUE/SDKs/HostLinux/Linux_x64/v22_clang-16.0.6-centos7/x86_64-unknown-linux-gnu/bin";
-        ueEditorBinPath = "${ueRootDirPath}/Engine/Binaries/Linux/UnrealEditor";
-        ueRootDirPath = "/mnt/storage/unreal_editors/ue5.4.1";
-        uprojectPath = "${pwd}/${projectName}.uproject";
+        projectName = "MonoGrinding";
+        unrealPath = "/mnt/storage/unreal_editors/ue5.4.1";
+        projectPath = "${pwd}/${projectName}.uproject";
       in {
         default = devenv.lib.mkShell {
           inherit inputs pkgs;
           modules = [
             {
-              # https://devenv.sh/reference/options/
               packages = with pkgs; [
                 clang-tools_16
-                clang_16
-                dotnet-sdk
-                libllvm
                 mach-nix-pkgs
-                python3
               ];
-
               scripts = {
-                openue.exec = ''
-                  nvidia-offload steam-run ${ueEditorBinPath} ${uprojectPath}
+                de-openue.exec = ''
+                  nvidia-offload openue ${unrealPath} ${projectPath}
                 '';
 
-                de-use_nix_bins.exec = ''
-                  mv ${ueBatchFilesDirPath}/SetupDotnet.sh ${ueBatchFilesDirPath}/SetupDotnet.sh.bak
-                  mv ${ueCentosBinariesDirPath}/clang++ ${ueCentosBinariesDirPath}/clang++.bak
-                  mv ${ueCentosBinariesDirPath}/llvm-ar ${ueCentosBinariesDirPath}/llvm-ar.bak
-
-                  cp ${devenvCustomDirPath}/SetupDotnet-Patched.sh ${ueBatchFilesDirPath}/SetupDotnet.sh
-                  cp ${devenvCustomDirPath}/system_clang++_wrapper ${ueCentosBinariesDirPath}/clang++
-                  cp ${devenvCustomDirPath}/system_llvm-ar_wrapper ${ueCentosBinariesDirPath}/llvm-ar
-                '';
-
-                de-use_ue_bins.exec = ''
-                  mv ${ueBatchFilesDirPath}/SetupDotnet.sh.bak ${ueBatchFilesDirPath}/SetupDotnet.sh
-                  mv ${ueCentosBinariesDirPath}/clang++.bak ${ueCentosBinariesDirPath}/clang++
-                  mv ${ueCentosBinariesDirPath}/llvm-ar.bak ${ueCentosBinariesDirPath}/llvm-ar
-                '';
-
-                de-uecli_gen.exec = ''
-                  de-use_nix_bins
-                  UE_USE_SYSTEM_DOTNET=1 ${devenvCustomDirPath}/uecli_gen_wrapper.sh
-                  de-use_ue_bins
-                '';
-
-                de-ubt_gen.exec = ''
-                  ${ueRootDirPath}/Engine/Build/BatchFiles/Linux/Build.sh -mode=GenerateClangDatabase \
-                    -project=${uprojectPath} ${projectName}Editor Development Linux -OutputDir=${pwd}
-                '';
-
-                de-debug-skipbuild.exec = ''
-                  de-use_nix_bins
-
-                  UE_USE_SYSTEM_DOTNET=1 ${ueRootDirPath}/Engine/Build/BatchFiles/Linux/Build.sh \
-                    ${projectName}Editor Linux DebugGame -SkipBuild -project=${uprojectPath}
-
-                  de-use_ue_bins
-                '';
-
-                gen.exec = ''
-                  running_msg="C++ Gen Running..."
-                  completed_msg="C++ Gen Completed!"
-
-                  uecli_gen_log_path="de-uecli_gen.log"
-                  skipbuild_log_path="de-debug-skipbuild.log"
-
-                  ${pkgs.libnotify}/bin/notify-send --icon=dialog-information "${projectName}" "$running_msg"
-                  echo "$running_msg"
-                  de-uecli_gen &> "$uecli_gen_log_path" || (cat "$uecli_gen_log_path" | grep --color error && false)
-                  de-debug-skipbuild &> "$skipbuild_log_path" || (cat "$skipbuild_log_path" | grep --color error && false)
-                  echo "$completed_msg"
-                  ${pkgs.libnotify}/bin/notify-send --icon=face-smile "${projectName}" "$completed_msg"
+                de-uegen.exec = ''
+                  uegen ${unrealPath} ${projectName} ${projectPath}
                 '';
               };
             }
